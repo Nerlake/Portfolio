fd99adbe33a1c9278574dd2b20e2f3a9c777d5c5
Nerlake <cdf.leo.hotz@gmail.com>
2023-03-14 15:24:54 +0100

    Feat: Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// event listener qui detecte si l'utilisateur est en haut de la page
+// window.addEventListener('scroll', function () {
+//     if (window.scrollY > 20) {
+//         document.querySelector('.header').style.backgroundColor = '#000000';
+//         document.querySelector('.header').style.boxShadow = "-1px 4px 15px -6px black";
+//     } else {
+//         // change le background de la classe .header en bleu
+//         document.querySelector('.header').style.backgroundColor = "rgba(0, 0, 0, 0)";
+//         document.querySelector('.header').style.boxShadow = "none";
+//     }
+// });
